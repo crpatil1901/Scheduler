d1a5b61a11d918865359c23f56468efa3d51506c
crpatil1901 <crpatil1901@gmail.com>
2022-10-27 14:00:25 +0530

    First Push

---
+++ Scheduler/ViewModels/ScheduleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleViewController.swift
+//  Scheduler
+//
+//  Created by Chinmay Patil on 18/10/22.
+//
+
+import SwiftUI
+
+struct ScheduleViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduleViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleViewController()
+    }
+}
